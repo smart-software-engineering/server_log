16e0642a65a0c9a0253708e184205672feadcb59
Rico Metzger <rico@gmail.com>
2024-05-23 18:31:25 +0200

    Fly deployment

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule ServerLog.MixProject do
     [
       {:phoenix, "~> 1.7.12"},
       {:phoenix_html, "~> 4.0"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_reload, "~> 1.2"},
       {:phoenix_live_view, "~> 0.20.2"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
